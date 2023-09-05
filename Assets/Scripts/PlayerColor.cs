f0df9a8982bbd6bb1f280f377904b2f1842e1762
yusufozdemir00 <ozdemiryusuf430@gmail.com>
2023-09-05 21:58:13 +0300

    Test
    Photon Fusion test is completed. ToDo-> new game testing

---
+++ Assets/Scripts/PlayerColor.cs
@@ -0,0 +1,23 @@
+﻿using Fusion;
+using UnityEngine;
+
+public class PlayerColor : NetworkBehaviour
+{
+    public MeshRenderer MeshRenderer;
+
+    [Networked(OnChanged = nameof(NetworkColorChanged))]
+    public Color NetworkedColor { get; set; }
+
+    private static void NetworkColorChanged(Changed<PlayerColor> changed)
+    {
+        changed.Behaviour.MeshRenderer.material.color = changed.Behaviour.NetworkedColor;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            NetworkedColor = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f), 1f);
+        }
+    }
+}
